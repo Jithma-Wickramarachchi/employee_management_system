5ccbcdc60ae729221e099faa51cc3524fd0aa2a8
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-08-07 13:47:12 +0530

    Create addEmployee() and getEmployeeList() in EmployeeServiceImpl

---
+++ src/main/java/com/teccience/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.teccience.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teccience.dto.EmployeeDto;
+import com.teccience.entitiy.EmployeeEntity;
+import com.teccience.repository.EmployeeRepository;
+import com.teccience.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository repository;
+    private final ObjectMapper mapper;
+    @Override
+    public EmployeeDto addEmployee(EmployeeDto dto) {
+        EmployeeEntity entity = mapper.convertValue(dto, EmployeeEntity.class);
+        return mapper.convertValue(repository.save(entity), EmployeeDto.class);
+    }
+
+    @Override
+    public List<EmployeeDto> getEmployeeList() {
+        List<EmployeeDto> employeeDtoList = new ArrayList<>();
+        List<EmployeeEntity> employeeEntityList = repository.findAll();
+        Iterator<EmployeeEntity> iterator = employeeEntityList.iterator();
+
+        while (iterator.hasNext()){
+            EmployeeDto dto = mapper.convertValue(iterator.next(), EmployeeDto.class);
+            employeeDtoList.add(dto);
+        }
+        return employeeDtoList;
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(EmployeeDto dto) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteEmployee(Integer id) {
+        return false;
+    }
+}
